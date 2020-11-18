4f3e536295dfba00076c8f1ee457f66edb9e8865
hryeong66 <jhr110326@gmail.com>
2020-11-18 20:41:33 +0900

    회원가입 구현

---
+++ src/web/DoJoin.java
@@ -0,0 +1,75 @@
+package web;
+
+
+import java.io.IOException;
+import java.sql.Connection;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import model.ManageUser;
+
+
+
+/**
+ * Servlet implementation class DoJoin
+ */
+@WebServlet("/doJoin")
+public class DoJoin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DoJoin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		ServletContext sc = getServletContext();
+		Connection conn= (Connection)sc.getAttribute("DBconnection");
+		if(conn == null) {
+			System.out.println("conn is nul");
+		}
+		
+		int result = ManageUser.addMember(conn, request);
+		
+		//아이디 중복 확인 코드 작성
+
+
+		//user 정보 DB에 넣기
+		
+		try {
+			if (result != -1) {
+			System.out.println("입력 성공");
+			RequestDispatcher view = request.getRequestDispatcher("index.html");
+			view.forward(request, response);
+			}
+			
+		}catch(Exception e){
+			
+		}
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
