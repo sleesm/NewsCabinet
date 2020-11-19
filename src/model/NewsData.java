435add8cc306d69910443569756d852b373a7490
SeongMinKwon <sleepingsm813@gmail.com>
2020-11-20 01:18:11 +0900

    refine Date - show news

--- src/model/NewsData.java
+++ src/model/NewsData.java
@@ -8,6 +8,8 @@ public class NewsData {
 		this.url = url;
 		this.description = description;
 		this.pubDate = pubDate;
+
+		refineDate(this.pubDate);
 	}
 
 	private String headline;
@@ -48,7 +50,49 @@ public class NewsData {
 	}
 	
 	private void refineDate(String pubDate) {
-		
+		String date[] = pubDate.split(" ");
+		setPubDate(date[3]+"년 " + translateMonth(date[2])+ date[1] + "일 " + translateDay(date[0]));
+	}
+	
+	private String translateMonth(String data) {
+		String month = null;
+		switch(data){
+			case "Jan": month = "1월 "; break;
+			case "Feb": month = "2월 "; break;
+			case "Mar": month = "3월 "; break;
+			case "Apr": month = "4월 "; break;
+			case "May": month = "5월 "; break;
+			case "June": month = "6월 "; break;
+			case "July": month = "7월 "; break;
+			case "Aug": month = "8월 "; break;
+			case "Sep": month = "9월 "; 
+			case "Sept": month = "9월 "; break;
+			case "Oct": month = "10월 "; break;
+			case "Nov": month = "11월 "; break;
+			case "Dec": month = "12월 "; break;
+			default: month = ""; break;
+		}
+		return month;
+	}
+	
+	private String translateDay(String data) {
+		String day = null;
+		switch(data){
+			case "Mon,": day = "월요일"; break;
+			case "Tu,": day = "화요일 ";
+			case "Tue,": day = "화요일 ";
+			case "Tues,": day = "화요일 ";break;
+			case "Wed,": day = "수요일"; break;
+			case "Th,": day = "목요일";
+			case "Thu,": day = "목요일";
+			case "Thur,": day = "목요일";
+			case "Thurs,": day = "목요일"; break;
+			case "Fri,": day = "금요일"; break;
+			case "Sat,": day = "토요일"; break;
+			case "Sun,": day = "일요일"; break;
+			default: day = ""; break;
+		}
+		return day;
 	}
 	
 }
