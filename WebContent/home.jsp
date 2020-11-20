af48266efc9713fbfa06bae805f6eec90e19614f
hryeong66 <jhr110326@gmail.com>
2020-11-20 16:28:45 +0900

    add login session

--- WebContent/home.jsp
+++ WebContent/home.jsp
@@ -4,9 +4,42 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<link href="css/style.css" rel="stylesheet">
+<title>Home</title>
 </head>
+<jsp:include page="newsHeader.html" />
 <body>
-	입력 테스트 성공
+	<div class="content-area">
+		<div class="wrapper">
+			<p>
+				<%
+					int signInCheck = (Integer) request.getAttribute("SignInCheck");
+					HttpSession userSession = request.getSession(false);
+					
+					if (signInCheck == 1) {
+						out.print("로그인에 성공하였습니다.");
+						if(session != null){
+							out.println(userSession.getAttribute("userId"));
+							out.println(userSession.getAttribute("userName"));
+							out.println(userSession.getAttribute("userCategory"));
+						}
+						
+					} else if (signInCheck == 0)
+							out.print("잘못된 비밀번호 입니다. 다시 시도해주세요");
+						else
+							out.print("없는 아이디입니다. 다시 시도해주세요");
+					
+					
+				%>
+
+				<a href="index.html"><input type="button" value="다시로그인하기"></a>
+
+			</p>
+		</div>
+	</div>
+	
+
+	
+			
 </body>
 </html>
---
+++ src/web/SignIn.java
@@ -0,0 +1,108 @@
+package web;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.ManageUser;
+
+/**
+ * Servlet implementation class DoLogin
+ */
+@WebServlet("/SignIn")
+public class SignIn extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SignIn() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String userID = request.getParameter("userid");
+		String userPW = request.getParameter("passwd");
+		int userCategory;
+		String userName; 
+		HttpSession session; 
+		
+		int check = -1;
+		
+		ServletContext sc = getServletContext();
+		Connection conn = (Connection) sc.getAttribute("DBconnection");
+		
+		ResultSet rs = ManageUser.searchUserByID(conn, userID);
+		
+		//ResultSet rs = ManageUser.searchUserPasswdByID(conn, userID);
+		
+		
+		if (rs != null) {
+			try {
+				if (rs.next()) {  // 해당 아이디가 있음
+					String checkpw = rs.getString(4);
+					if(userPW.equals(checkpw)) { //로그인 성공
+						check = 1;
+						System.out.println("로그인 성공");
+						session = request.getSession();
+						userCategory = rs.getInt(2);
+						userName = rs.getString(3);
+						System.out.println(userID + userCategory + userName);
+						session.setAttribute("userId", userID);
+						session.setAttribute("userCategory", userCategory);
+						session.setAttribute("userName", userName);
+						if(session.isNew()) {
+							System.out.println("------session 생성됨");
+							userCategory = rs.getInt(2);
+							userName = rs.getString(3);
+							System.out.println(userID + userCategory + userName);
+							session.setAttribute("userId", userID);
+							session.setAttribute("userCategory", userCategory);
+							session.setAttribute("userName", userName);
+						}
+					}
+					else {
+						check = 0;
+						System.out.println("비밀번호 틀림");
+					}
+				}
+				else{//아이디가 없음 
+					check = -1;
+					System.out.println("아이디없음");
+				}	
+			} catch (Exception e) {
+				e.printStackTrace();
+			} // try end
+		} 
+		
+		request.setAttribute("SignInCheck", check);
+		RequestDispatcher view = request.getRequestDispatcher("home.jsp");
+		view.forward(request, response);
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
